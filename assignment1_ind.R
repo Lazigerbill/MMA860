c6e5fcbed2c59e64c22abd435702a2ab2c00639f
Lazigerbill <chaukbill@gmail.com>
2019-06-28 22:16:28 -0400

    part b completed

--- assignment1_ind.R
+++ assignment1_ind.R
@@ -4,6 +4,7 @@
 library(tidyverse)
 library(dplyr)
 library(readxl)
+library(lubridate)
 
 # Load data into dataframe
 spec_df <- read_excel("/Users/localadmin/Downloads/MMA860_Assignment1_Data_vf.xlsx", sheet = 1)
@@ -11,7 +12,7 @@ loc_df <- read_excel("/Users/localadmin/Downloads/MMA860_Assignment1_Data_vf.xls
 weath_df <- read_excel("/Users/localadmin/Downloads/MMA860_Assignment1_Data_vf.xlsx", sheet = 3)
 
 
-#split weather data
+#split and clean weather data
 info <- weath_df[0:9, 0:2]
 
 clean_df <- tail(weath_df, -16)
@@ -31,3 +32,15 @@ cal_air_den <- function(press, temp){
 
 clean_df <- mutate(clean_df, air_den = cal_air_den(clean_df$stn.press..kpa., clean_df$temp...c.))
 
+# b) Convert windspeed to m/s
+
+# check for errors in windspeed and convert to numeric
+colnames(clean_df)
+sum(sapply(clean_df$wind.spd..km.h., is.na))
+clean_df$wind.spd..km.h. <- as.numeric(clean_df$wind.spd..km.h.)
+
+convert_ms <- function(kmh){
+	return(kmh*0.277778)
+}
+
+clean_df <- mutate(clean_df, wind_spd_ms = convert_ms(clean_df$wind.spd..km.h.))
